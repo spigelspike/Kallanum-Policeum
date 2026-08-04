453f32d91f9be4898aa6272e3d9293bcb73ba4d6
spigelspike <shareefch1413@gmail.com>
2026-08-05 01:54:08 +0530

    feat: add Vercel Analytics tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import SignUpPage from './components/lobby/SignUpPage'
 import HomeScreen from './components/lobby/HomeScreen'
 import GameRoom from './components/game/GameRoom'
@@ -7,15 +8,18 @@ import RoomNotFound from './components/common/RoomNotFound'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignUpPage />} />
-        <Route path="/home" element={<HomeScreen />} />
-        <Route path="/room/:code" element={<GameRoom />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/not-found" element={<RoomNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUpPage />} />
+          <Route path="/home" element={<HomeScreen />} />
+          <Route path="/room/:code" element={<GameRoom />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/not-found" element={<RoomNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   )
 }
 
